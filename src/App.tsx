7aa6992ad64d981c7aa8982565e79931b7b902bc
Emely Pleitez <epleitez@belaytech.com>
2025-06-23 10:32:33 -0400

    add radio button examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import './App.css';
+import MuiRadioButton from './components/MuiRadioButton';
 // import MuiTypography from './components/MuiTypography';
 // import { MuiButton } from "./components/MuiButton";
-//import MuiTextField from "./components/MuiTextField";
-import MuiSelect from "./components/MuiSelect";
+// import MuiTextField from "./components/MuiTextField";
+// import MuiSelect from "./components/MuiSelect";
+
+
 
 function App() {
   return (
     <div className="App">
-      {/* <MuiTypography />
-      <MuiButton /> */}
+      {/* <MuiTypography /> */}
+      {/* <MuiButton /> */}
       {/* <MuiTextField /> */}
+      {/* <MuiSelect /> */}
 
-      <MuiSelect />
+      <MuiRadioButton />
 
 
     </div>
