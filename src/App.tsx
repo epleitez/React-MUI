930141439edfdc89320013251a0b9078c8fa318b
Emely Pleitez <epleitez@belaytech.com>
2025-06-23 13:58:05 -0400

    add checkbox functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
-import MuiRadioButton from './components/MuiRadioButton';
+import { MuiCheckbox } from './components/MuiCheckbox';
 // import MuiTypography from './components/MuiTypography';
 // import { MuiButton } from "./components/MuiButton";
 // import MuiTextField from "./components/MuiTextField";
 // import MuiSelect from "./components/MuiSelect";
+//import MuiRadioButton from './components/MuiRadioButton';
 
 
 
@@ -14,9 +15,8 @@ function App() {
       {/* <MuiButton /> */}
       {/* <MuiTextField /> */}
       {/* <MuiSelect /> */}
-
-      <MuiRadioButton />
-
+      {/* <MuiRadioButton /> */}
+      <MuiCheckbox />
 
     </div>
   );
