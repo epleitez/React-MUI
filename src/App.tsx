e4f81a65379c45fd334c34a18808187e90ab414f
Emely Pleitez <epleitez@belaytech.com>
2025-06-23 14:09:31 -0400

    switch component functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import { MuiCheckbox } from './components/MuiCheckbox';
+import MuiSwitch from './components/MuiSwitch';
+// import { MuiCheckbox } from './components/MuiCheckbox';
 // import MuiTypography from './components/MuiTypography';
 // import { MuiButton } from "./components/MuiButton";
 // import MuiTextField from "./components/MuiTextField";
@@ -16,7 +17,8 @@ function App() {
       {/* <MuiTextField /> */}
       {/* <MuiSelect /> */}
       {/* <MuiRadioButton /> */}
-      <MuiCheckbox />
+      {/* <MuiCheckbox /> */}
+      <MuiSwitch />
 
     </div>
   );
